e4ee45325a91e6cffd2f7607db88d2872fd29ef5
mariannacoppa <densimetra@gmail.com>
2024-09-17 15:25:11 +0200

    [UPD] created class with 3 variables

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-    echo 'ciao!';
+    //inizializzo la classe Movie nella quale dichiaro 3 variabili
+    class Movie {
+        public $title;
+        public $genre;
+        public $year;
+    };
 ?>
