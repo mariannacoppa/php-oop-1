ce8a2391c6fde35e3cc4378ea073af4661d9b8ad
mariannacoppa <densimetra@gmail.com>
2024-09-17 16:10:54 +0200

    [UPD] printed property values FINE ESERCIZIO BASE

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 
         //definisco un costruttore
         function __construct($title, $genre, $year) {
-            $this->title = $_title;
-            $this->genre = $_genre;
-            $this->year = $_year;
+            $this->title = $title;
+            $this->genre = $genre;
+            $this->year = $year;
         }
 
         // definisco due metodi
@@ -17,12 +17,26 @@
             return $this->title;
         }
         function getMovieDetails() {
-            return $this->title." ".$this->genre." ".$this->year;
+            return $this->genre." ".$this->year;
         }
 
-        //definisco 3 oggetti e invoco il costruttre
-        $avatar = new Movie('Avatar', 'Fantascienza', '2009');
-        $big = new Movie('Big', 'Commedia', '1988');
-        $CastAway = new Movie('Cast Away', 'Drammatico', '2000');
     }
+    
+    //definisco 3 oggetti e invoco il costruttre
+    $avatar = new Movie('Avatar', 'Fantascienza', '2009');
+    $big = new Movie('Big', 'Commedia', '1988');
+    $CastAway = new Movie('Cast Away', 'Drammatico', '2000');
+
+    //stampo i valori delle proprietà dei 3 oggetti
+    echo $avatar -> getMovieTitle();
+    echo '<br>';
+    echo $avatar -> getMovieDetails();
+    echo '<hr>';
+    echo $big -> getMovieTitle();
+    echo '<br>';
+    echo $big -> getMovieDetails();
+    echo '<hr>';
+    echo $CastAway -> getMovieTitle();
+    echo '<br>';
+    echo $CastAway -> getMovieDetails();
 ?>
