6199f2c29290bd47bf4c199c2dd0b7e178f5b1ab
mariannacoppa <densimetra@gmail.com>
2024-09-17 18:07:43 +0200

    [FIX] deleted spaces next to arrow operators

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+    include './bonus/server.php';
     //inizializzo la classe Movie nella quale dichiaro 3 variabili
     class Movie {
         public $title;
-        public $genre;
+        public $genres;
         public $year;
 
         //definisco un costruttore
-        function __construct($_title, $_genre, $_year) {
+        function __construct($_title, Array $_genres, $_year) {
             $this->title = $_title;
-            $this->genre = $_genre;
+            $this->genres = $_genres;
             $this->year = $_year;
         }
 
@@ -17,26 +18,26 @@
             return $this->title;
         }
         function getMovieDetails() {
-            return $this->genre." ".$this->year;
+            return $this->genres." ".$this->year;
         }
 
     }
     
     //definisco 3 oggetti e invoco il costruttre
-    $avatar = new Movie('Avatar', 'Fantascienza', '2009');
-    $big = new Movie('Big', 'Commedia', '1988');
-    $CastAway = new Movie('Cast Away', 'Drammatico', '2000');
+    $avatar = new Movie('Avatar', ['Fantascienza', 'Drammatico'], '2009');
+    $big = new Movie('Big', ['Commedia', 'Drammatico'], '1988');
+    $CastAway = new Movie('Cast Away', ['Drammatico', 'Avventura'], '2000');
 
     //stampo i valori delle proprietà dei 3 oggetti
-    echo $avatar -> getMovieTitle();
+    echo $avatar->getMovieTitle();
     echo '<br>';
-    echo $avatar -> getMovieDetails();
+    echo $avatar->getMovieDetails();
     echo '<hr>';
-    echo $big -> getMovieTitle();
+    echo $big->getMovieTitle();
     echo '<br>';
-    echo $big -> getMovieDetails();
+    echo $big->getMovieDetails();
     echo '<hr>';
-    echo $CastAway -> getMovieTitle();
+    echo $CastAway->getMovieTitle();
     echo '<br>';
-    echo $CastAway -> getMovieDetails();
+    echo $CastAway->getMovieDetails();
 ?>
