42a4c07e8c08a1ab009ccbb94756daef9d685c56
mariannacoppa <densimetra@gmail.com>
2024-09-17 15:32:38 +0200

    [UPD] defined constructor

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
         public $title;
         public $genre;
         public $year;
+
+        //definisco un costruttore
+        function __construct($title, $genre, $year) {
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->year = $_year;
+        }
     };
 ?>
